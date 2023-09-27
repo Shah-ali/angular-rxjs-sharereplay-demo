ae5e379872b3c3c65a9e45ffef7e838610651b14
Shah-ali <ali.shahnwz@live.com>
2023-09-27 14:20:10 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,21 @@ import { AppService } from './app.service';
   template: `
     <div *ngIf="user$ | async as user">
       <h1>User Details</h1>
-      <p>Name: {{ user.name }}</p>
-      <p>Email: {{ user.email }}</p>
+      <p>Name: {{ users.name }}</p>
+      <p>Email: {{ users.email }}</p>
     </div>
   `,
 })
 export class AppComponent {
   user$;
+  users: any;
 
   constructor(private appSerice: AppService) {}
 
   ngOnInit() {
     this.user$ = this.appSerice.getUserData();
+    this.user$.subscribe((result) => {
+      this.users = result;
+    });
   }
 }
